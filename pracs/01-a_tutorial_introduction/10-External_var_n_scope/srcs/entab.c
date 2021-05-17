6b30c047ec05988c83c7d2ca5b54e18fa73ca49b
junehan <junehan.dev@gmail.com>
2021-05-17 13:34:54 +0900

    NEW/entab/finished

--- pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/entab.c
+++ pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/entab.c
@@ -1,11 +1,46 @@
-#include <assert.h>
-#include <unistd.h>
-#define MAXLEN 2048
+#include "ft_extern.h"
 
-char	line[MAXLINE];
+int		entab(char *src, int nth);
+int		is_entab(char *src);
 
 int		main(void)
 {
+	int			i;
+	char		ch;
+	extern char	line[];
 
+	while (get_line()) {
+		i = 0;
+		
+		while ((ch = *(line + i))) {
+			if (ch == ' ' && is_entab(line + i))
+				entab(line, i);
+			printf("%c: %d\n", *(line + i), *(line + i));
+			i++;
+		}
+	}
+}
+
+int		is_entab(char *src)
+{
+	int	i;
+
+	i = 3;
+	while (i)
+		if (*(src + i--) != ' ')
+			return (0);
+	return (1);
+}
+
+int		entab(char *src, int nth)
+{
+	char	buf[MAXLINE];
+
+	copy(buf, (src + nth + 4));
+	copy((src + nth), "\t\0");
+	copy(src + nth + 1, buf);
+	assert(*(src + nth) == '\t');
+	assert(*(buf) == *(src + nth + 1));
+	return (nth + 1);
 }
 
