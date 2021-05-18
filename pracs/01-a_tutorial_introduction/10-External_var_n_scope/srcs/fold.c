8461d1f3e4e63c3d72c0073e1d7d8f3e2ab08f61
junehan <junehan.dev@gmail.com>
2021-05-18 15:50:29 +0900

    NEW/fold/next_paragraph
    - column definition excludes new line pattern.

--- pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/fold.c
+++ pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/fold.c
@@ -1,27 +1,39 @@
 #include <unistd.h>
 #include <assert.h>
 /*
- * Macros
- */
-#define	is_blank(ch) ((ch == ' ' || ch == '\t') ? 1 : 0);
-/*
  * Forward functions
  */
-ssize_t next_column(const char *src);
-ssize_t next_nonblank(const char *src);
+ssize_t		next_column(const char *src);
+ssize_t		next_nonblank(const char *src);
+const char	*next_paragraph(const char *src);
 
 int		main(void)
 {
 	return (0);
 }
 
+const char	*next_paragraph(const char *src)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(src + i) && *(src + i) != '\n')
+		i++;
+
+	if (!*(src + i) || !*(src + i + 1))
+		return (NULL);
+	return (src + i + 1);
+}
+
 ssize_t next_nonblank(const char *src)
 {
 	ssize_t	i;
+	char	ch;
 
 	i = 0;
-	while (*(src + i)) {
-		if (!is_blank(*(src + i)))
+
+	while ((ch = *(src + i))) {
+		if (ch != ' ' || ch != '\t' || ch != '\n')
 			return (i);
 		i++;
 	}
@@ -42,11 +54,9 @@ ssize_t next_column(const char *src)
 	if (ch == '.') {
 		while ((ch = *(src + i)) != '.')
 			i++;
-	} else if (ch == '\n') {
-		while ((ch = *(src + i)) != '\n')
-			i++;
 	} else
 		i = -1;
+
 	return (i);
 }
 
