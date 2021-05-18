b4ae68b1a1294e4dd6c61966be8ec4f2a8a6d172
junehan <junehan.dev@gmail.com>
2021-05-18 16:30:25 +0900

    TEST/fold/NEEDFIX
    FIX required
       - next_paragraph should handle end of file,
       - next_paragraph return value is not easy to handle

--- pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/fold.c
+++ pracs/01-a_tutorial_introduction/10-External_var_n_scope/srcs/fold.c
@@ -1,10 +1,27 @@
 #include <unistd.h>
 #include <assert.h>
+#include <ctype.h>
 #include "ft_extern.h"
+#define NL	'\n'
 
-int		main(void)
+
+int		main(int argc, const char *argv[])
 {
+	const char	*pg;
+	int			counts[1];
+
+	assert(argc == 2);
 	
+	counts[0] = 0;
+	pg = next_paragraph(argv[1]);
+	
+	while ((pg = next_paragraph(pg)) != NULL) {
+		assert(isalnum((int)*pg));
+		assert(*(pg - 1) == NL);
+		counts[0]++;
+		fstring("----pg: %d----\n", counts);
+	}
+	assert(pg == NULL); 
 	return (0);
 }
 
