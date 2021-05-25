0a85f9eb61114a4f882d9fb6157df7963d7ad24c
junehan <junehan.dev@gmail.com>
2021-05-25 18:41:15 +0900

    NEW/uncomment.comment_chr
    - search offset for comment start

--- pracs/01-a_tutorial_introduction/uncomment/srcs/main.c
+++ pracs/01-a_tutorial_introduction/uncomment/srcs/main.c
@@ -13,12 +13,33 @@ int		main(int argc, const char *argv[])
 	len = strlen(origin);
 	remove_comment(origin);
 	last = 0;
+	
 	while (i < len) {
 		while (!origin[i] && i < len)
 			i++;
 		origin[last++] = origin[i++];
 	}
 	write(STDOUT_FILENO, origin, strlen(origin));
+}
+
+ssize_t	comment_chr(char *src, )
+{
+	char		*src_pt;
+	static char qute = 0;
+
+	src_pt = src;
+	while (*src_pt) {
+		if (*src_pt == '/' && qute) {
+			if (*(++src_pt) == '*')
+				return (src_pt - src);
+		} else if (*src_pt == '\\' && *(src_pt + 1 == '"')){
+			src_pt += 2;
+		} else if (*src_pt == '"') {
+			qute = ~qute;
+		} else
+			src_pt++;
+	}
+	return (-1);
 }
 
 char	*remove_comment(char *src)
@@ -28,19 +49,23 @@ char	*remove_comment(char *src)
 	static char	qute = 0;
 
 	i = 0;
-	while (ch = *(src + i)) {
-		if (ch == '"')
+	while ((ch = *(src + i))) {
+		if (ch == '"') {
 			qute = ~qute;
-		else if (!qute) {
-			if (ch == '/' && *(src + i + 1) == '*') {
-				while (*(src + i) != '*' && *(src + i + 1) != '/' && *(src + i))
-				 	*(src + i++) = '\0';
-				if (*(src + i))
-					*(src + i++) = '\0';
-				*(src + i) = '\0';
+			i++;
+		} else {
+			if (!qute && ch == '/') {
+				if (*(src + i + 1) == '*') {
+					while (*(src + i) != '*' && *(src + i + 1) != '/' && *(src + i))
+					 	*(src + i++) = 'a';
+					if (*(src + i))
+						*(src + i++) = 'a';
+					*(src + i) = '\0';
+				}
+			} else {
+				i++;
 			}
 		}
-		i++;
 	}
 	return (src);
 }
