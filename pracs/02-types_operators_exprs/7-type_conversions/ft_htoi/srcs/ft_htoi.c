fb0064cbfbb6d156445f225b264ced5f4cfe0f2f
junehan <junehan.dev@gmail.com>
2021-06-01 16:50:21 +0900

    Implemented/2.7/htoi
    - ft_htoi was implemented
    - main Proc to run code

--- pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/srcs/ft_htoi.c
+++ pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/srcs/ft_htoi.c
@@ -1,7 +1,5 @@
 int	ft_ishex(int c)
 {
-	const char hexvals = "0123456789abcdef";
-
 	if (c >= '0' && c <= '9')
 		return (c - '0');
 	
@@ -13,5 +11,38 @@ int	ft_ishex(int c)
 
 int	ft_htoi(const char *src)
 {
-	
+	int	ret;
+	int	i;
+	int	v;
+
+	ret = 0;
+	i = 0;
+	if (*src == '-' || *src ==  '+')
+		i++;
+
+	while ((v = ft_ishex(*(src + i))) != -1) {
+		ret *= 16;
+		ret += v;
+		i++;
+	}
+	if (*src == '-')
+		ret = ~ret + 1;
+
+	return (ret);
+}
+
+#include <stdio.h>
+int	main(int argc, const char *argv[])
+{
+	int	ret;
+
+	if (argc == 2) {
+		ret = ft_htoi(argv[1]);
+		printf("Input:\t%s\n", argv[1]);
+		printf("integer:\t%d\n", ret);
+		printf("tohex:\t%x\n", ret);
+		return (0);	
+	}
+
+	return (0);
 }
