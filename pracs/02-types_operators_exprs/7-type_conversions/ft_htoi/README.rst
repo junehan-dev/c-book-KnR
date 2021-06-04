45acff988523d3a24cd93538815a079c52bd2360
junehan <junehan.dev@gmail.com>
2021-06-04 16:47:57 +0900

    ENHENCED/2.7/htoi
    - Covers string start with "0x".
    - consider 'a' and 'A' as equal value.

--- pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/README.rst
+++ pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/README.rst
@@ -1,8 +1,19 @@
 ft_htoi
 =======
 
-ft_ishex
+ft_tohex
 --------
+``int	to_hex(int c);``
+   A function convert character *c* to interger in hex.
+   Returns 0 ~ 15, case when ``[a-fA-F0-9]`` in *c.*
+   On Non Hex convertable, returns ``-1`` to discriminate it.
 
 ft_htoi
 -------
+``int	ft_htoi(const char *src);``
+   A function convert string(hex string) to integer .
+   Covers - or + mark prepended.
+   Uses ``ft_tohex`` internally.
+   Returns integer converted from string in hex.
+   If nothing converted or *'0'* only in ``*src``
+   then, returns 0.
--- pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/srcs/ft_htoi.c
+++ pracs/02-types_operators_exprs/7-type_conversions/ft_htoi/srcs/ft_htoi.c
@@ -1,12 +1,25 @@
-int	ft_ishex(int c)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_htoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/04 16:31:12 by junehan           #+#    #+#             */
+/*   Updated: 2021/06/04 16:47:23 by junehan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_tohex(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (c - '0');
-	
 	if (c >= 'a' && c <= 'f')
 		return (c - 'a' + 10);
-
-	return (-1);
+	else if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	else if (c >= '0' && c <= '9')
+		return (c - '0');
+	else
+		return (-1);
 }
 
 int	ft_htoi(const char *src)
@@ -17,20 +30,25 @@ int	ft_htoi(const char *src)
 
 	ret = 0;
 	i = 0;
+
 	if (*src == '-' || *src ==  '+')
 		i++;
 
-	while ((v = ft_ishex(*(src + i))) != -1) {
+	if (*(src + i) == '0' && *(src + i + 1) == 'x') 
+		i += 2;
+
+	while ((v = ft_tohex(*(src + i))) != -1) {
 		ret *= 16;
 		ret += v;
 		i++;
 	}
+
 	if (*src == '-')
 		ret = ~ret + 1;
 
 	return (ret);
 }
-
+/*
 #include <stdio.h>
 int	main(int argc, const char *argv[])
 {
@@ -38,11 +56,15 @@ int	main(int argc, const char *argv[])
 
 	if (argc == 2) {
 		ret = ft_htoi(argv[1]);
-		printf("Input:\t%s\n", argv[1]);
+		printf("Input:\t\t%s\n", argv[1]);
 		printf("integer:\t%d\n", ret);
-		printf("tohex:\t%x\n", ret);
+		printf("tohex:\t\t%x\n", ret);
+		printf("-21 to hex = %x\n", -21);
+		printf("33 to hex = %x\n", 33);
 		return (0);	
 	}
 
 	return (0);
 }
+*/
+
