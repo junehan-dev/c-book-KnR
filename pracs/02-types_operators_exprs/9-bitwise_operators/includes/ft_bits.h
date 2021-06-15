387df02232d3b7b827275124252b6544de02f514
junehan <junehan.dev@gmail.com>
2021-06-15 16:39:57 +0900

    REFACTOR/2.9/CLEAN/#52
    - header updated
    - code formated

--- pracs/02-types_operators_exprs/9-bitwise_operators/includes/ft_bits.h
+++ pracs/02-types_operators_exprs/9-bitwise_operators/includes/ft_bits.h
@@ -1,4 +1,8 @@
 #ifndef __FT_BITS_H__
 #define __FT_BITS_H__
+#include <unistd.h>
 unsigned int	ft_setbits(unsigned int src, size_t offset, size_t len, int mask);
+unsigned int	ft_invert(unsigned int src, size_t offset, size_t len, size_t maxlen);
+int				ft_rightrot(int src, size_t cnt);
 #endif
+
--- pracs/02-types_operators_exprs/9-bitwise_operators/srcs/ft_invert.c
+++ pracs/02-types_operators_exprs/9-bitwise_operators/srcs/ft_invert.c
@@ -6,19 +6,19 @@
 /*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 12:09:51 by junehan           #+#    #+#             */
-/*   Updated: 2021/06/15 12:13:49 by junehan          ###   ########.fr       */
+/*   Updated: 2021/06/15 16:39:14 by junehan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-unsigned int ft_invert(unsigned int src, size_t offset, size_t len, size_t maxlen)
+unsigned int	ft_invert(unsigned int src, size_t offset, size_t len, size_t maxlen)
 {
-	unsigned int left_end;
-	unsigned int mid_end;
-	unsigned int ret;
-	unsigned int mid_mask;
-	unsigned int mid_inverted;
+	unsigned int	left_end;
+	unsigned int	mid_end;
+	unsigned int	ret;
+	unsigned int	mid_mask;
+	unsigned int	mid_inverted;
 
 	if (!len || (offset + len) > maxlen)
 		return (src);
