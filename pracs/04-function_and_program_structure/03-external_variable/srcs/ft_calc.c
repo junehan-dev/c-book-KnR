73136c8091553037e8c492a64c59972440c37a91
junehan <junehan.dev@gmail.com>
2021-07-13 12:48:53 +0900

    TEST/DBC/4-3/ft_calc.main/ONGOING
    - PRECONDITION
       - token get count
       - end of program

--- pracs/04-function_and_program_structure/03-external_variable/srcs/ft_calc.c
+++ pracs/04-function_and_program_structure/03-external_variable/srcs/ft_calc.c
@@ -6,10 +6,26 @@
 /*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 14:47:59 by junehan           #+#    #+#             */
-/*   Updated: 2021/07/12 14:48:00 by junehan          ###   ########.fr       */
+/*   Updated: 2021/07/13 12:48:44 by junehan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "ft_calc.h"
 
+int		main(int argc, const char **argv)
+{
+	const char	*token_pt;
+	
+	
+	token_pt = (argc == 2) ? ft_gettoken(*(argv + 1)) : (const char *)0;
+	while (token_pt) {
+		write(STDOUT_FILENO, "START\n", 6);
+		write(STDOUT_FILENO, token_pt, 1);
+		token_pt = ft_gettoken(token_pt + 1);
+	}
+
+	write(STDOUT_FILENO, "\nEXIT\n", 6);
+	return (0);
+}
 
