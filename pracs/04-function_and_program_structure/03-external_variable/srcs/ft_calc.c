4d8d5ad883fc10ede1e91f2a672b8ad14c02519b
junehan <junehan.dev@gmail.com>
2021-07-13 17:31:55 +0900

    NEW/DBC/4-3/ft_calc.set_tokens
    - set_tokens parses the set of num + oper + num.
    - Test for input validated in simple way.

--- pracs/04-function_and_program_structure/03-external_variable/srcs/ft_calc.c
+++ pracs/04-function_and_program_structure/03-external_variable/srcs/ft_calc.c
@@ -6,26 +6,84 @@
 /*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 14:47:59 by junehan           #+#    #+#             */
-/*   Updated: 2021/07/13 12:48:44 by junehan          ###   ########.fr       */
+/*   Updated: 2021/07/13 17:31:20 by junehan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "ft_calc.h"
+
+ssize_t	intlen(const char *src)
+{
+	size_t ret;
+
+	ret = 0;
+	if (ft_isdigit(src)) {
+		ret = (*src == '-' || *src == '+') ? 1 : 0;
+		while (*(src + ret) >= '0' && *(src + ret) <= '9')
+			ret++;
+	}
+
+	return (ret);
+}
+
+size_t	set_tokens(const char *src)
+{
+	const char	*number;
+	const char	*operator;
+	const char	*src_pt;
+	size_t	ret;
+
+	ret = 0;
+	operator = 0;
+	number = ft_gettoken(src);
+	//add_number(number);
+	write(STDOUT_FILENO, number, intlen(number));
+	src_pt = number + intlen(number);
+	
+	while ((operator = ft_gettoken(src_pt))) {
+		/*
+		assert(ft_isoperator(operator));
+		add_operator(operator);
+		*/
+		if (!ft_isoperator(*operator))
+			return (0);
+
+		write(STDOUT_FILENO, operator, 1);
+		number = ft_gettoken(operator + 1);
+		if (!ft_isdigit(number))
+			return (0);
+
+		write(STDOUT_FILENO, number, intlen(number));
+		/*
+		assert(ft_isdigit(number));
+		add_number(number);
+		*/
+		src_pt = number + intlen(number);
+		ret++;
+	}
+
+	if (operator)
+		ret = 0;
+
+	return (ret);
+}
 
 int		main(int argc, const char **argv)
 {
-	const char	*token_pt;
-	
-	
-	token_pt = (argc == 2) ? ft_gettoken(*(argv + 1)) : (const char *)0;
-	while (token_pt) {
-		write(STDOUT_FILENO, "START\n", 6);
-		write(STDOUT_FILENO, token_pt, 1);
-		token_pt = ft_gettoken(token_pt + 1);
-	}
+	const char	*input;
+	int			operator_count;
 
-	write(STDOUT_FILENO, "\nEXIT\n", 6);
-	return (0);
+	input = (argc == 2) ? *(argv + 1) : 0;
+	if (input) {
+		operator_count = ft_isdigit(input) ? set_tokens(input): 0;
+		if (operator_count < 1) {
+			write(STDOUT_FILENO, "INPUT ERROR\n", 12);
+			return (EXIT_FAILURE);
+		}
+	}
+	write(STDOUT_FILENO, "EXIT\n", 6);
+	return (EXIT_SUCCESS);
 }
 
