d316cef44caeb0912b1adc9db3d3582694556f3a
junehan <junehan.dev@gmail.com>
2021-07-26 15:51:25 +0900

    4-4/ft_stack/REFACTOR/Substitute if/fs_push
    - if statement converted to ternary operator

--- 4-4_stack/srcs/ft_stack_push.c
+++ 4-4_stack/srcs/ft_stack_push.c
@@ -6,7 +6,7 @@
 /*   By: junehan <junehan.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 11:49:16 by junehan           #+#    #+#             */
-/*   Updated: 2021/07/26 10:22:38 by junehan          ###   ########.fr       */
+/*   Updated: 2021/07/26 15:51:10 by junehan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@ int	ft_stack_push(void **stack, void *data)
 	if (!data)
 		return (-1);
 
-	if (!*stack && !*(stack + 1))
-		*stack = data;
-
+	*stack = (!*(stack) && !*(stack + 1)) ? data : *stack;
 	return ((*stack == data) ? 0 : 1);
 }
 
