0f8331bcc22bfd2b1040ed500df43926e53f571f
junehan <junehan.dev@gmail.com>
2021-11-01 17:56:14 +0900

    HOTFIX/5/5/example/my_strcmp/indentation fixed

--- examples/5-5_strcmp.c
+++ examples/5-5_strcmp.c
@@ -11,15 +11,16 @@ int	strcmp(const char *s1, const char *s2)
 
 int main(int argc, const char *argv[])
 {
-	int			ret;
+	int	ret;
 	
 	if (argc != 3)
 		return (1);
 
 	ret = strcmp(argv[1], argv[2]);
-	!ret ? printf("equal\n") : (
-		(ret > 0) ?  printf("s1 bigger %d\n", ret): printf("s2 bigger %d\n", ret)
-	);
+	!ret ?
+		printf("equal\n") : ((ret > 0) ?
+			printf("s1 bigger %d\n", ret) : printf("s2 bigger %d\n", ret)
+		);
 
 	return (0);
 }
