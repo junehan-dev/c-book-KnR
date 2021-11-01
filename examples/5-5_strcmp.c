5c3c67096f8717b8c11761d666a6897e22e7a10d
junehan <junehan.dev@gmail.com>
2021-11-01 17:48:21 +0900

    5/5/String Pointers and Functions/KeyNote/FIN

---
+++ examples/5-5_strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int	strcmp(const char *s1, const char *s2)
+{
+	if (*s1 && *s1 == *s2)
+		while (*++s1 == *++s2 && *s1)
+			;
+
+	return (*s1 - *s2);
+}
+
+int main(int argc, const char *argv[])
+{
+	int			ret;
+	
+	if (argc != 3)
+		return (1);
+
+	ret = strcmp(argv[1], argv[2]);
+	!ret ? printf("equal\n") : (
+		(ret > 0) ?  printf("s1 bigger %d\n", ret): printf("s2 bigger %d\n", ret)
+	);
+
+	return (0);
+}
